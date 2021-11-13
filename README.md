44ae889921d1d868104b4851b4a221c2be637b77
MrLutik <yaievgeniy@gmail.com>
2021-11-13 17:04:36 +0200

    essentials through array. first attempt

--- README.md
+++ README.md
@@ -3,10 +3,7 @@ KIRA Infrastructure Deployment &amp; Monitoring Manager
 
 ### Setup instructions:
 ```
-sudo -s
-
-cd /tmp && CHAINID="$(TERM=ansi whiptail --inputbox 'Please, enter existing or new chain id' 8 78 master --title 'KM2.0 Setup' 3>&1 1>&2 2>&3)" && \
- wget https://raw.githubusercontent.com/KiraCore/kira-manager/feature/init_shell/init.sh -O ./i.sh &>/dev/null && clear && \
+cd $HOME/tmp && wget https://raw.githubusercontent.com/KiraCore/kira-manager/feature/init_shell/init.sh -O ./i.sh &>/dev/null && clear && \
  chmod 555 -v ./i.sh &>/dev/null && H="$(sha256sum ./i.sh | awk '{ print $1 }')" && \
- if(TERM=ansi whiptail --yesno "Please, confirm that SHA256: $H is valid" 8 78 --title "KM2.0 Setup"); then ./i.sh $CHAINID; else clear; echo "INFO: Setup was cancelled by the user."; exit 1; fi
+ if(TERM=ansi whiptail --yesno "Please, confirm that SHA256: $H is valid" 8 78 --title "KM2.0 Setup"); then ./i.sh ; else clear; echo "INFO: Setup was cancelled by the user."; exit 1; fi
 ```
--- init.sh
+++ init.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
 
-echo "Arguments received $@"
+GO_VERSION="1.15.11"
+GOROOT="/usr/local/go"
+GOPATH="/home/go"
+GOCACHE="/home/go/cache"
+GOBIN="${GOROOT}/bin"
+ARCHITECTURE=$(uname -m)
 
+while true; do
+PSWD="$(whiptail --title 'KM2.0 Setup' --passwordbox 'Enter your root password to continue installation.' 10 60 3>&1 1>&2 2>&3)"
+
+exitstatus=$?
+if [[ $exitstatus != 0 ]] || [[ -z $PSWD ]]; then
+    whiptail --title "KM2.0 Setup" --msgbox "Operation canceled" 10 60 
+else
+    whiptail --title "KM2.0 Setup" --msgbox "Success" 10 60
+    break
+fi
+done
+
+declare -A ESSENTIALS
+ESSENTIALS["update"]="sudo apt get update -y <<< $PSWD"
+ESSENTIALS["golang_wget"]="wget https://dl.google.com/go/go$GO_VERSION.linux-$GOLANG_ARCH.tar.gz" 
+ESSENTIALS["golang_tar"]="tar -C /usr/local -xvf go$GO_VERSION.linux-$GOLANG_ARCH.tar.gz"
+ESSENTIALS["docker_remove"]="sudo apt-get remove docker docker-engine docker.io containerd runc -y <<< $PSWD"
+ESSENTIALS["docker_depend"]="sudo apt-get install ca-certificates curl gnupg lsb-release -y <<< $PSWD"
+ESSENTIALS["docker_gpg"]="curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg <<< $PSWD"
+ESSENTIALS["docker_set_stable"]="echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
+                                     https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null <<< $PSWD"
+{
+i=0
+for key in ${!ESSENTIALS[@]}; do
+    sleep 2
+    echo "${key} ${ESSENTIALS[${key}]}" >> log
+    echo $i
+    i+=1
+done
+} | whiptail --gauge "Installing essentials..." 6 50 0
