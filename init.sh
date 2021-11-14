244c61145442f72eaa1954a6fa872f1287f0c409
MrLutik <yaievgeniy@gmail.com>
2021-11-14 18:43:31 +0200

    fix >dev null

--- init.sh
+++ init.sh
@@ -40,7 +40,7 @@ ESSENTIALS[2]="sudo tar -C /usr/local -xvf go$GO_VERSION.linux-$GOLANG_ARCH.tar.
 ESSENTIALS[3]="sudo apt-get remove docker docker-engine docker.io containerd runc -y <<< $PSWD"
 ESSENTIALS[4]="sudo apt-get install ca-certificates curl gnupg lsb-release -y <<< $PSWD"
 ESSENTIALS[5]="curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg <<< $PSWD"
-ESSENTIALS[6]="echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null <<< $PSWD"
+ESSENTIALS[6]="echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list <<< $PSWD"
 
 ESSENTIALS_LEN=${#ESSENTIALS[@]}
 
