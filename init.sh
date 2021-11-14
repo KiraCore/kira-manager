dc407d7ef9a2fd2c40e600b48667a14a842fd721
MrLutik <yaievgeniy@gmail.com>
2021-11-14 22:08:15 +0200

    eval wild 1011

--- init.sh
+++ init.sh
@@ -42,7 +42,7 @@ ESSENTIALS[2]="sudo -S -k <<< $PSWD tar -C /usr/local -xvf go$GO_VERSION.linux-$
 ESSENTIALS[3]="sudo -S -k <<< $PSWD apt-get remove docker docker-engine docker.io containerd runc -y >> log"
 ESSENTIALS[4]="sudo -S -k <<< $PSWD apt-get install ca-certificates curl gnupg lsb-release -y >> log"
 ESSENTIALS[5]="curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo -S -k <<< $PSWD gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg >> log"
-ESSENTIALS[6]="sudo -S -k <<< $PSWD tee /etc/apt/sources.list.d/docker.list <<< 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable'"
+ESSENTIALS[6]="sudo -S -k <<< \"$PSWD\" tee /etc/apt/sources.list.d/docker.list <<< 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable'"
 
 ESSENTIALS_LEN=${#ESSENTIALS[@]}
 
