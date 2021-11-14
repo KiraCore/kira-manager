41e9cc85571c8f9199b10fe3728b70d561767276
MrLutik <yaievgeniy@gmail.com>
2021-11-14 17:19:12 +0200

    WARNING! eval added

--- init.sh
+++ init.sh
@@ -19,21 +19,35 @@ else
 fi
 done
 
+declare -A ESSENTIALS_NAMES
+ESSENTIALS_NAMES[0]="update"
+ESSENTIALS_NAMES[1]="golang_wget"
+ESSENTIALS_NAMES[2]="golang_tar"
+ESSENTIALS_NAMES[3]="docker_remove"
+ESSENTIALS_NAMES[4]="docker_depend"
+ESSENTIALS_NAMES[5]="docker_gpg"
+ESSENTIALS_NAMES[6]="docker_set_stable"
+
 declare -A ESSENTIALS
-ESSENTIALS["update"]="sudo apt get update -y <<< $PSWD"
-ESSENTIALS["golang_wget"]="wget https://dl.google.com/go/go$GO_VERSION.linux-$GOLANG_ARCH.tar.gz" 
-ESSENTIALS["golang_tar"]="tar -C /usr/local -xvf go$GO_VERSION.linux-$GOLANG_ARCH.tar.gz"
-ESSENTIALS["docker_remove"]="sudo apt-get remove docker docker-engine docker.io containerd runc -y <<< $PSWD"
-ESSENTIALS["docker_depend"]="sudo apt-get install ca-certificates curl gnupg lsb-release -y <<< $PSWD"
-ESSENTIALS["docker_gpg"]="curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg <<< $PSWD"
-ESSENTIALS["docker_set_stable"]="echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
-                                     https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null <<< $PSWD"
-{
-i=0
-for key in ${!ESSENTIALS[@]}; do
-    sleep 2
-    echo "${key} ${ESSENTIALS[${key}]}" >> log
-    echo $i
-    i+=1
-done
+ESSENTIALS[0]="sudo apt get update -y <<< $PSWD"
+ESSENTIALS[1]="wget https://dl.google.com/go/go$GO_VERSION.linux-$GOLANG_ARCH.tar.gz" 
+ESSENTIALS[2]="tar -C /usr/local -xvf go$GO_VERSION.linux-$GOLANG_ARCH.tar.gz"
+ESSENTIALS[3]="sudo apt-get remove docker docker-engine docker.io containerd runc -y <<< $PSWD"
+ESSENTIALS[4]="sudo apt-get install ca-certificates curl gnupg lsb-release -y <<< $PSWD"
+ESSENTIALS[5]="curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg <<< $PSWD"
+ESSENTIALS[6]="echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null <<< $PSWD"
+
+ESSENTIALS_LEN=${#ESSENTIALS[@]}
+
+{ 
+    for (( i=0; i<${ESSENTIALS_LEN}; i++ )) ; do
+
+        COUNTER=$((100/$ESSENTIALS_LEN*$i)) 
+        echo "$COUNTER"
+        echo "${ESSENTIALS[$i]}" >> log 
+        eval "${ESSENTIALS[$i]}"
+
+    done 
+
 } | whiptail --gauge "Installing essentials..." 6 50 0
+
