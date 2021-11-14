ba633ad3d5f27b1c4a11484ef53d6568cf58eedd
MrLutik <yaievgeniy@gmail.com>
2021-11-14 22:46:44 +0200

    added: docker install, text to installation screen

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+#To avoid password leakage
 export HISTIGNORE='*sudo -S*'
+
+USER=$(logname)
 
 GO_VERSION="1.15.11"
 GOROOT="/usr/local/go"
@@ -15,7 +19,7 @@ else
 fi
 
 while true; do
-PSWD="$(whiptail --title 'KM2.0 Setup' --passwordbox 'Enter your root password to continue installation.' 10 60 3>&1 1>&2 2>&3)"
+PSWD="$(whiptail --title 'KM2.0 Setup' --passwordbox 'Enter your $USER password to continue installation.' 10 60 3>&1 1>&2 2>&3)"
 
 exitstatus=$?
 if [[ $exitstatus != 0 ]] || [[ -z $PSWD ]]; then
@@ -27,13 +31,14 @@ fi
 done
 
 declare -A ESSENTIALS_NAMES
-ESSENTIALS_NAMES[0]="update"
-ESSENTIALS_NAMES[1]="golang_wget"
-ESSENTIALS_NAMES[2]="golang_tar"
-ESSENTIALS_NAMES[3]="docker_remove"
-ESSENTIALS_NAMES[4]="docker_depend"
-ESSENTIALS_NAMES[5]="docker_gpg"
-ESSENTIALS_NAMES[6]="docker_set_stable"
+ESSENTIALS_NAMES[0]="Updating packages..."
+ESSENTIALS_NAMES[1]="Downloading golang..."
+ESSENTIALS_NAMES[2]="Unpacking golang..."
+ESSENTIALS_NAMES[3]="Removing old docker packges..."
+ESSENTIALS_NAMES[4]="Installing docker dependencies..."
+ESSENTIALS_NAMES[5]="Handling gpg..."
+ESSENTIALS_NAMES[6]="Seting docker repo..."
+ESSENTIALS_NAMES[7]="Installing docker..."
 
 declare -A ESSENTIALS
 ESSENTIALS[0]="sudo -S <<< \"$PSWD\" apt-get update -y >> log"
@@ -43,6 +48,7 @@ ESSENTIALS[3]="sudo -S -k <<< \"$PSWD\" apt-get remove docker docker-engine dock
 ESSENTIALS[4]="sudo -S -k <<< \"$PSWD\" apt-get install ca-certificates curl gnupg lsb-release -y >> log"
 ESSENTIALS[5]="curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo -S -k <<< \"$PSWD\" gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg >> log"
 ESSENTIALS[6]="sudo -S -k <<< \"$PSWD\" tee /etc/apt/sources.list.d/docker.list <<< 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable'"
+ESSENTIALS[7]="sudo -S -k <<< \"$PSWD\" apt-get install docker-ce docker-ce-cli containerd.io"
 
 ESSENTIALS_LEN=${#ESSENTIALS[@]}
 
@@ -50,11 +56,11 @@ ESSENTIALS_LEN=${#ESSENTIALS[@]}
     for (( i=0; i<${ESSENTIALS_LEN}; i++ )) ; do
 
         COUNTER=$((100/$ESSENTIALS_LEN*$i)) 
-        echo "$COUNTER"
+        echo -e "XXX\n$COUNTER\\${ESSENTIALS_NAMES[$i]}\nXXX"
         echo "${ESSENTIALS[$i]}" >> log 
         eval $(echo "${ESSENTIALS[$i]}") &>eval.log
 
     done 
 
-} | whiptail --gauge "Installing essentials..." 6 50 0
+} | whiptail --title 'KM2.0 Setup' --gauge "Installing essentials..." 6 50 0
 
