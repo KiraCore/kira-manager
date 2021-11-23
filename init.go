3f468035e898b4fcb9d716417d58fda46f03520d
MrLutik <yaievgeniy@gmail.com>
2021-11-23 22:29:07 +0200

    docker setup. check todo

--- init.go
+++ init.go
@@ -12,20 +12,34 @@ import (
 const (
 	KM_VERSION string = "KM2.0.0"
 )
-
+/*
+type Menu struct{
+}
+type Init struct {
+	*Menu
+}
+*/
 func CheckCmd(cmd_name string) bool {
 	if _, err := exec.LookPath(cmd_name); err != nil {
-
 		return false
 	}
 	return true
 }
 func InstallDocker() (err error) {
+	//Probalby will need to break for smaller peaces
+	//Idea: args with exec.Command add to struct. Wrap execution of exec.Command to add counter. Counter can be used as a gauge in whiptail.
 	var buff bytes.Buffer
+
+	upd := exec.Command("apt-get", "update", "-y")
+	if err = upd.Run(); err != nil {
+		log.Fatalln("Failed to update")
+	}
+
 	req := exec.Command("apt-get", "install", "apt-transport-https", "ca-certificates", "curl", "software-properties-common", "-y")
 	if err = req.Run(); err != nil {
 		log.Fatalln("Failed to install required dependencies")
 	}
+	//Pipe for adding GPG
 	gpg := exec.Command("curl", "-fsSL", "https://download.docker.com/linux/ubuntu/gpg")
 	add_gpg := exec.Command("apt-key", "add", "-")
 	r, w := io.Pipe()
@@ -37,10 +51,31 @@ func InstallDocker() (err error) {
 	w.Close()
 	add_gpg.Wait()
 	io.Copy(os.Stdout, &buff)
+
+	add_repo := exec.Command("add-apt-repository", "\"deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable\"")
+	if err = add_repo.Run(); err != nil {
+		log.Fatalln("Failed to add docker repositoriy")
+	
+	upd = upd.Run()
+	if err = upd.Run(); err != nil {
+		log.Fatalln("Failed to update")
+	}
+
+	apt_cache:= exec.Command("apt-cache", "policy", "docker-ce","-y")
+	if err = apt_cache.Run(); err != nil {
+		log.Fatalln("Failed to update")
+	}
+
+	install_docker:=exec.Command("apt","install", "docker-ce","-y")
+	if err = install_docker.Run(); err != nil {
+		log.Fatalln("Failed to update")
+	}
 	return err
 }
 
+//IDEA: Should try to use named pypes to wrap whiptail
 /*
+
 func SetupNode(name string) (err error) {
 	if CheckCmd("docker") != true {
 		err := InstallDocker()
@@ -50,8 +85,8 @@ func SetupNode(name string) (err error) {
 func Menu() (choice string) {
 	var stderr bytes.Buffer
 	menu := exec.Command("whiptail", "--title", KM_VERSION, "--menu", "Please, choose the node you want to install", "16", "60", "2",
-		"1.", "Setup validator node",
-		"2.", "Setup sentry node",
+		"1.", "Validator node",
+		"2.", "Sentry node",
 	)
 
 	menu.Stdout = os.Stdout // redirect stdout
---
+++ node/sentry.go
@@ -0,0 +1,9 @@
+package node
+
+import (
+	"fmt"
+)
+
+func not_main() {
+	fmt.Println("Blocks/funcs to run sekaid in container")
+}
