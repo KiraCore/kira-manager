7d6a179c09683a85e2112a71a61ddde390d8edc9
MrLutik <yaievgeniy@gmail.com>
2021-11-23 21:05:25 +0200

    fix/test

--- init.go
+++ init.go
@@ -23,28 +23,30 @@ func CheckCmd(cmd_name string) bool {
 func InstallDocker() (err error) {
 	var buff bytes.Buffer
 	req := exec.Command("apt-get", "install", "apt-transport-https", "ca-certificates", "curl", "software-properties-common", "-y")
-	err = req.Run()
-
+	if err = req.Run(); err != nil {
+		log.Fatalln("Failed to install required dependencies")
+	}
 	gpg := exec.Command("curl", "-fsSL", "https://download.docker.com/linux/ubuntu/gpg")
 	add_gpg := exec.Command("apt-key", "add", "-")
-	r, w: = io.Pipe()
-	gpg.Stdout = w
-	add_gpg.Stdin = r
-	add_gpg = &buff
+	r, w := io.Pipe()
+	gpg.Stdout, add_gpg.Stdin = w, r
+	add_gpg.Stdout = &buff
 	gpg.Start()
 	add_gpg.Start()
 	gpg.Wait()
 	w.Close()
 	add_gpg.Wait()
 	io.Copy(os.Stdout, &buff)
+	return err
 }
 
+/*
 func SetupNode(name string) (err error) {
 	if CheckCmd("docker") != true {
 		err := InstallDocker()
 	}
 }
-
+*/
 func Menu() (choice string) {
 	var stderr bytes.Buffer
 	menu := exec.Command("whiptail", "--title", KM_VERSION, "--menu", "Please, choose the node you want to install", "16", "60", "2",
@@ -70,6 +72,9 @@ func main() {
 	switch input := Menu(); input {
 	case "1.":
 		fmt.Println("installing validator")
+		if err := InstallDocker(); err != nil {
+			log.Fatalln("Failed to install docker")
+		}
 	case "2.":
 		fmt.Println("inslling sentry")
 	}
