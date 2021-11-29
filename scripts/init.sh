14957814e3a605e4230ce4718ddbc09afddec32b
MrLutik <yaievgeniy@gmail.com>
2021-11-29 14:43:01 +0200

    testing/aws

--- scripts/init.sh
+++ scripts/init.sh
@@ -27,6 +27,7 @@ installEssentials () {
         if [[ $err != 0 ]]; then 
         whiptail --title "KM2.0 Setup" --msgbox "Installation of essentials faield..." 10 60
         exit 1
+        fi
     done
 }
 
