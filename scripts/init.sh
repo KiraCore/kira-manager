bac76f8ab89a729420d9db74bf6018dcc9ef0df6
MrLutik <yaievgeniy@gmail.com>
2021-11-29 14:56:25 +0200

    testing/aws

--- scripts/init.sh
+++ scripts/init.sh
@@ -22,7 +22,7 @@ installEssentials () {
         COUNTER=$((100/$ESSENTIALS_LEN*$i)) 
         echo -e "XXX\n$COUNTER\n${ESSENTIALS_OPERATIONS[$i]}\nXXX"
         #echo "${ESSENTIALS[$i]}" >> log 
-        eval $(echo "${ESSENTIALS[$i]}") > /dev/null
+        eval $(echo "${ESSENTIALS[$i]}") >>/dev/null 2>&1
         err=$?
         [[ $err != 0 ]] && whiptail --title "KM2.0 Setup" --msgbox "Installation of essentials faield..." 10 60 && exit 1
 
